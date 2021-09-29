45df79eec9b449094694f7e5144b07393160fb90
haelx <alentandome@hotmail.com>
2021-09-28 22:17:02 -0400

    Cambios de diseño en la vista login

--- frontend/login-vista.php
+++ frontend/login-vista.php
@@ -51,6 +51,7 @@
             <a type="button" class="botonPrincipal btn btn-dark" href="./index.php">Regresar Pagina Principal</a>
         </div>
     </div>
+    <div class="col-lg-6 col-md-6 d-none d-md-block image-container"></div>
 </div>
 </div>
 <script src="../../recursos/jquery-3.1.1.min.js" type="text/javascript"></script>
